f6087be701ab90b92d52c3e7f5b43deff4a1635c
Vagner Oliveira <ti.vagner@gmail.com>
2021-03-01 21:05:40 -0300

    Configura Devise para Admin e User

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
 require "active_storage/engine"
+require 'action_cable/engine'
 
 # require "rails/test_unit/railtie"
 
