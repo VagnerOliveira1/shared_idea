e002e28e4efe19d5154d6af69ecbf1c152851d82
Vagner Oliveira <ti.vagner@gmail.com>
2021-03-23 17:23:57 -0300

    Refatora Controller

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -1,24 +1,35 @@
 class AdminsBackoffice::AdminsController < AdminsBackofficeController
+  before_action :password_vefiry, only: [:update]
+  before_action :set_admin, only: [:edit, :update]
+
   def index
     @admins = Admin.all
   end
 
   def edit
-    @admin = Admin.find(params[:id])
   end
 
   def update
-    if params[:admin][:password].blank? && params[:admin][:password_confirmation].blank?
-      params[:admin].extract!(:password, :password_confirmation)
-    end
-
-    @admin  = Admin.find(params[:id])
-    params_admin = params.require(:admin).permit(:email, :password, :password_confirmation)
-
-    if @admin.update(params_admin)
+    if @admin.update(admin_params)
       redirect_to admins_backoffice_admins_path, notice: "Administrador atualizado com sucesso!"
     else
       render :edit
+    end
+  end
+
+  private
+
+  def set_admin
+    @admin = Admin.find(params[:id])
+  end
+
+  def admin_params
+    params_admin = params.require(:admin).permit(:email, :password, :password_confirmation)
+  end
+
+  def password_vefiry
+    if params[:admin][:password].blank? && params[:admin][:password_confirmation].blank?
+      params[:admin].extract!(:password, :password_confirmation)
     end
   end
 
