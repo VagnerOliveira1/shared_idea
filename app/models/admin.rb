46e15f5901990fe3ef95019d5b1d06501bf0908d
Vagner Oliveira <ti.vagner@gmail.com>
2021-03-24 16:19:01 -0300

    Adiciona paginação com Kaminari

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,4 +3,8 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+#Kaminari default page
+  paginates_per 2
+
 end
