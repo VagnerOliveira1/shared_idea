de46b8d12d26903e849354e680b9a8a23e721e9f
Vagner Oliveira <ti.vagner@gmail.com>
2021-03-25 17:25:23 -0300

    Cria Views para Questões e refatora
    Remove Subjects.all da view e cria um método no controller para realizar essa busca

---
+++ app/controllers/admins_backoffice/questions_controller.rb
@@ -0,0 +1,56 @@
+class AdminsBackoffice::QuestionsController < AdminsBackofficeController
+  before_action :set_question, only: [:edit, :update, :destroy]
+  before_action :subjects_all, only: [:new, :edit]
+
+  def index
+    @questions = Question.all.page(params[:page])
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save!
+      redirect_to admins_backoffice_questions_path, notice: "Questão criada com sucesso!"
+    else
+      render :new
+    end
+  end
+
+  def edit;end
+
+  def update
+    if @question.update(question_params)
+      redirect_to admins_backoffice_questions_path, notice: "Questão atualizada com sucesso!"
+    else
+      render :edit
+    end
+  end
+
+
+  def destroy
+    if @question.destroy!
+      redirect_to admins_backoffice_questions_path, notice: "Questão excluída com sucesso!"
+    else
+      render :index
+    end
+  end
+
+
+  private
+
+  def set_question
+    @question = Question.find(params[:id])
+  end
+
+  def question_params
+    params_question = params.require(:question).permit(:content, :subject_id)
+  end
+
+  def subjects_all
+    Subject.all.order(:description)
+  end
+
+end
